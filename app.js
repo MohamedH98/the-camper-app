730912849828533af7059e7d885b07d1b57b8ba9
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-04-30 20:27:19 +0100

    update package.json and port

--- app.js
+++ app.js
@@ -149,6 +149,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(8000, () => {
-  console.log("Listening on server 8000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
