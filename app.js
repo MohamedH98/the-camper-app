34aea141ee08ecd039e57ed876b416b22f9b7407
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-04-30 23:18:16 +0100

    refactor app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const path = require("path");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -17,7 +18,7 @@ const usersRoutes = require("./routes/users");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
-const DBURL = process.env.DBURL;
+const DBURL = process.env.DBURL || "mongodb://localhost:27017/yelp-camp";
 const { SECRET } = process.env;
 const MongoDBStore = require("connect-mongo");
 mongoose.connect(DBURL, {
@@ -34,7 +35,6 @@ db.once("open", () => {
 });
 
 const app = express();
-const path = require("path");
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -42,6 +42,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 const store = MongoDBStore.create({
   mongoUrl: DBURL,
@@ -69,7 +74,6 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
-app.use(mongoSanitize());
 app.use(helmet());
 
 const scriptSrcUrls = [
@@ -149,7 +153,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
