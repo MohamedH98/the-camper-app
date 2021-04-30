c0479a6f9614ec83024f83b5ab7817763024f088
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-05-01 00:13:54 +0100

    fix styles  rendering

--- app.js
+++ app.js
@@ -77,20 +77,20 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com",
+  "https://stackpath.bootstrapcdn.com/",
   "https://api.tiles.mapbox.com",
   "https://api.mapbox.com",
-  "https://kit.fontawesome.com",
-  "https://cdnjs.cloudflare.com",
-  "https://cdn.jsdelivr.net",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
 ];
 const styleSrcUrls = [
-  "https://kit-free.fontawesome.com",
-  "https://stackpath.bootstrapcdn.com",
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
   "https://api.mapbox.com",
   "https://api.tiles.mapbox.com",
-  "https://fonts.googleapis.com",
-  "https://use.fontawesome.com",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
 ];
 const connectSrcUrls = [
   "https://api.mapbox.com",
